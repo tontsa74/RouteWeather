1ee1924c11bb589c11305b3026685047a0819120
tontsa74 <toni.silventoinen@tuni.fi>
2019-12-02 21:45:02 +0200

    install react-native-maps init Map component

---
+++ components/Map.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Dimensions, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class Map extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView 
+          style ={styles.mapStyle}
+        />
+      </View>
+    );
+  }
+ 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
