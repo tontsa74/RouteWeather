02ac6f3cac3210e07d2c9556b6c6201b37e49ebc
tontsa74 <toni.silventoinen@tuni.fi>
2020-01-04 08:42:33 +0200

    add RouteLeg model

---
+++ models/RouteLeg.js
@@ -0,0 +1,9 @@
+export class RouteLeg {
+  constructor(key, distance, duration, endCoord, startCoord) {
+    this.key = key;
+    this.distance = distance;
+    this.duration = duration;
+    this.endCoord = endCoord;
+    this.startCoord = startCoord;
+  }
+}
